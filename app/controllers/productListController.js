ee8486f3c2bd26b66b3a3a4479ef8d9ee6ad6eb4
Nidhin Narayanan <nidhin.narayanan@ad.infosys.com>
2018-09-02 20:19:13 +0530

    Add to cart code implemented

--- app/controllers/productListController.js
+++ app/controllers/productListController.js
@@ -13,5 +13,53 @@ hbiApp.controller('productListController', ['$scope','$http', 'productlistServic
 	$scope.setProduct = function(id) {
 		headerService.sessionSet("productId",id);
 	}
+	
+	$scope.quickAdd = function(action, productId,varient,quantity){
+		cartActions(action, productId,varient,quantity);
+	}
+	
+	function initCart() {
+		if (headerService.sessionGet('cart') == null) {
+			createCart();
+		}
+	}
     
+	// get custId now hard coded
+	function getCustId() {
+		var customerId = "3fd76661-6529-48bf-879e-5f5e126f0d98"; // need to change this.
+	}
+
+	// create cart
+	function createCart() {
+		var data = {};
+		var cart = {};
+		data.currency = 'GBP'; // get from header
+		data.customerId = getCustId(); // change this to custId after login implementation
+		productlistService.createCart().then(function(response) {
+			cart.id = resp.id;
+			cart.version = resp.version;
+			headerService.sessionSet('cart', cart)
+		});
+	}
+	
+	function cartActions(action, productId, varirantId, qty) {
+    initCart();
+    var cart = headerService.sessionGet('cart');
+    var input = {};
+    input.version = cart.version;
+    var actions = [];
+    var lineActions = {};
+    lineActions.action = action;
+    lineActions.productId = productId;
+    lineActions.variantId = varirantId;
+    lineActions.quantity = qty;
+    actions.push(lineActions);
+    input.actions = actions;
+	productlistService.cartActions(cart,input).then(function(response) {
+		cart.id = resp.id;
+		cart.version = resp.version;
+		headerService.sessionSet('cart', cart);
+	});
+}
+	
 }]);
--- app/services/productlistService.js
+++ app/services/productlistService.js
@@ -4,7 +4,7 @@ hbiApp.factory('productlistService', function($http, headerService) {
     getProducts: function (categoryId) { 
 				  return $http({
 						method: 'GET',
-						url: "https://api.sphere.io/hnb-59/product-projections/search?filter=categories.id: subtree(\""+categoryId+"\")",
+						url: "https://api.sphere.io/hnb-59/product-projections/search?filter=categories.id: subtree(\"a8f9d134-7007-453e-8f04-83fdd1a67b92\")",
 						headers: {
 							"Authorization": headerService.sessionGet("configData").header
 						},
@@ -17,6 +17,31 @@ hbiApp.factory('productlistService', function($http, headerService) {
 				  return $http({
 						method: 'GET',
 						url: "https://api.sphere.io/hnb-59/categories",
+						headers: {
+							"Authorization": headerService.sessionGet("configData").header
+						},
+						complete: function() {
+							console.log('done');
+						}
+					})
+    },
+	createCart: function () {
+				  return $http({
+						method: 'GET',
+						url: "https://api.sphere.io/hnb-59/carts",
+						headers: {
+							"Authorization": headerService.sessionGet("configData").header
+						},
+						complete: function() {
+							console.log('done');
+						}
+					})
+    },
+	cartActions: function (cart, input) {
+				  return $http({
+						method: 'GET',
+						url: "https://api.sphere.io/hnb-59/carts/" + cart.id,
+						params: input,
 						headers: {
 							"Authorization": headerService.sessionGet("configData").header
 						},

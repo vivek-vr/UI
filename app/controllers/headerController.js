d738a7a2b166c8b389cea12b6dac03c7cec36cf9
vivek-vr <vivek_velappannair@infosys.com>
2018-09-02 10:14:24 +0530

    header file changes for dynamic category

--- app/controllers/headerController.js
+++ app/controllers/headerController.js
@@ -3,15 +3,28 @@ hbiApp.controller('headerController', ['$scope', 'productlistService','headerSer
 	$scope.init = function(){
 		setAuthorizationCode();
 		var finalItemsList = [];
+		var outputList = [];
+		var parentLevelList = [];
+		var firstLevelList = [];
+		var secondLevelList = [];
 		var categories = productlistService.getCategories()  // get all categories to list
 		.then(function(response) {
 			angular.forEach(response.data.results, function(item, key1) {
 				if(item.ancestors.length == 0){
-					finalItemsList.push(item);
+					parentLevelList.push(item);
+				} else if(item.ancestors.length == 1){
+					firstLevelList.push(item);
+				}else if(item.ancestors.length == 2){
+					secondLevelList.push(item);
 				}
-				else if(item.ancestors.length == 1){
+			});
+			finalItemsList = parentLevelList.concat(firstLevelList).concat(secondLevelList);
+			angular.forEach(finalItemsList, function(item, key1) {
+				if(item.ancestors.length == 0){
+					outputList.push(item);
+				} else if(item.ancestors.length == 1){
 					angular.forEach(item.ancestors, function(ancestor, key2) {
-						angular.forEach(finalItemsList, function(parentItem, key1) {
+						angular.forEach(outputList, function(parentItem, key1) {
 							if(!parentItem.children){
 								parentItem.children = [];
 							}
@@ -20,24 +33,23 @@ hbiApp.controller('headerController', ['$scope', 'productlistService','headerSer
 							}	
 														
 						});
-				});
-			}else if(item.ancestors.length == 2){
-				angular.forEach(item.ancestors, function(ancestor, key2) {
-					angular.forEach(finalItemsList, function(firstLevelItem, key3) {
-						angular.forEach(firstLevelItem.children, function(secondLevelItem, key4) {
-							if(!secondLevelItem.children){
-								secondLevelItem.children = [];
-							}
-							if(ancestor.id == secondLevelItem.id){
-								secondLevelItem.children.push(item);
-							}														
+					});
+				}else if(item.ancestors.length == 2){
+					angular.forEach(item.ancestors, function(ancestor, key2) {
+						angular.forEach(outputList, function(firstLevelItem, key3) {
+							angular.forEach(firstLevelItem.children, function(secondLevelItem, key4) {
+								if(!secondLevelItem.children){
+									secondLevelItem.children = [];
+								}
+								if(ancestor.id == secondLevelItem.id){
+									secondLevelItem.children.push(item);
+								}														
+							});
 						});
 					});
-					});
-			}
-			
-		});
-		$scope.menuTree = finalItemsList; 
+				}
+			});
+		$scope.menuTree = outputList; 
 		
 	})
 	}
